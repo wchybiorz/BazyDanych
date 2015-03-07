31fbef1b43a2c12a7458fd32a4e2bc93fcaed31d
wchybiorz <wchybiorz@gmail.com>
2015-03-07 09:40:21 +0100

    Update ZaawansowaneQueriesJednaTabela.sql

--- ZaawansowaneQueriesJednaTabela.sql
+++ ZaawansowaneQueriesJednaTabela.sql
@@ -1,1 +1,2 @@
---
+SELECT LCASE(MID(CustomerName,1,1)) AS literka, COUNT(CustomerID) AS ile FROM Customers GROUP BY MID(CustomerName,1,1) ORDER BY MID(CustomerName,1,1) DESC
+-- wypisuje ilość nazwisk na daną literę (litera jest zamieniana na małą - LCASE) i sportuje malejąco
